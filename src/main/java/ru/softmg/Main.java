5bd6883f805ce2a3ec8eda55cae425155819e8cd
Vladimir Alekseev <167650711+aleksvmir@users.noreply.github.com>
2025-01-26 20:50:37 +0300

    Update Main.java

--- src/main/java/ru/softmg/Main.java
+++ src/main/java/ru/softmg/Main.java
@@ -2,7 +2,7 @@ package ru.softmg;
 
 public class Main {
 
-    private static String msg = ""Hello qa.guru!!!!"";
+    private static String msg = "Hello qa.guru!";
 
     public static void main(String[] args) {
         System.out.println(msg);
